333750ead1e0a61c1ab4447d982d45d50713e763
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2025-11-15 11:01:45 +0000

    fix: removed required flag

--- internal/mcmod-releaser/model/app.go
+++ internal/mcmod-releaser/model/app.go
@@ -89,5 +89,5 @@ func (v *AppVersion) loadFromBuildInfo() {
 }
 
 type AppConfig struct {
-	CurseForgeAPIToken string `env:"CURSEFORGE_API_TOKEN,required"`
+	CurseForgeAPIToken string `env:"CURSEFORGE_API_TOKEN"`
 }
